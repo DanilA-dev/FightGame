a58c0c4d26abd1c1680fe2abfbc120a5ef606217
DanilA-dev <78906212+DanilA-dev@users.noreply.github.com>
2023-12-16 17:54:27 +0300

    Montser asset, Player View

--- MultiplayerProject/Assets/Scripts/Core/PlayerController.cs
+++ MultiplayerProject/Assets/Scripts/Core/PlayerController.cs
@@ -6,10 +6,15 @@ namespace Player
     {
         [SerializeField] private float _moveSpeed;
         [SerializeField] private float _rotateSpeed;
+        [SerializeField] private float _smoothTime = 0.25f;
+        [Space] 
+        [SerializeField] private PlayerView _playerView;
 
         private PlayerInputSetter _playerInputSetter;
         private Rigidbody _rigidbody;
         private Vector3 _moveDir;
+        private float _currentAnimatorVelocity;
+        private float _velocity;
 
         private void Awake()
         {
@@ -22,6 +27,7 @@ namespace Player
         private void Update()
         {
             RotateToMoveDir();
+            UpdateCurrentSpeed();
         }
 
         private void FixedUpdate()
@@ -47,6 +53,15 @@ namespace Player
         private void UpdateMoveDirection(Vector2 input)
         {
             _moveDir = input != Vector2.zero ? new Vector3(input.x, 0, input.y) : Vector3.zero;
+        }
+
+        private void UpdateCurrentSpeed()
+        {
+            _currentAnimatorVelocity = _moveDir != Vector3.zero
+                ? Mathf.SmoothDamp(_currentAnimatorVelocity, _moveDir.magnitude, ref _velocity, _smoothTime)
+                : Mathf.SmoothDamp(_currentAnimatorVelocity, 0, ref _velocity, _smoothTime);
+            
+            _playerView.UpdateBlendTree(_currentAnimatorVelocity);
         }
     }
 }
---
+++ MultiplayerProject/Assets/Scripts/Core/PlayerView.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Player
+{
+    public class PlayerView : MonoBehaviour
+    {
+        [SerializeField] private Animator _animator;
+
+        private readonly int _moveHash = Animator.StringToHash("Speed");
+
+        public void UpdateBlendTree(float value)
+        {
+            _animator.SetFloat(_moveHash, value);
+        }
+    }
+}
